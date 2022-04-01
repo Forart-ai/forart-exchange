49e4996d9beac4ec652cbdc4ac23f2a3df57261e
monicaHsiao <615763004@qq.com>
2022-04-01 16:38:15 +0800

    update to mainnet

--- src/hooks/programs/useDonation/constants/index.ts
+++ src/hooks/programs/useDonation/constants/index.ts
@@ -4,7 +4,7 @@ export const POOL_ADDRESS = new PublicKey('F6gwyCP8awjU3oog18UbeXcA7W6z8Qis8utYv
 
 export const DONATE_PROGRAM_ID = new PublicKey('tJCoBgdxRUQi6VsUV1ubaWMVkRY9eBASpQhvP9ykrTq')
 
-export const USDC_TOKEN_ADDRESS = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr')
+export const USDC_TOKEN_ADDRESS = new PublicKey('EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v')
 
 export const USDC_TOKEN_DECIMALS = 6
 
