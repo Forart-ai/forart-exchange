0a57550ce5be14adf37524ec018dc41a9829fe6d
monicaHsiao <615763004@qq.com>
2022-04-02 16:29:05 +0800

    switch to mainnet-beta

--- src/hooks/programs/useDonation/constants/index.ts
+++ src/hooks/programs/useDonation/constants/index.ts
@@ -1,19 +1,20 @@
 import { PublicKey } from '@solana/web3.js'
 
-// export const POOL_ADDRESS = new PublicKey('EZ8osJZoSrqeVDMBzPw4CjA3Q1W7aVh8nWPz14vuUooN')
-//
-// export const DONATE_PROGRAM_ID = new PublicKey('tJCoBgdxRUQi6VsUV1ubaWMVkRY9eBASpQhvP9ykrTq')
-//
-// export const USDC_TOKEN_ADDRESS = new PublicKey('EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v')
-//
-// export const USDC_TOKEN_DECIMALS = 6
-
-export const POOL_ADDRESS = new PublicKey('sVA1BdvuBp5xCEUvnNT9uzjVvoQrN77D5nnBKQFEpP1')
+export const POOL_ADDRESS = new PublicKey('EZ8osJZoSrqeVDMBzPw4CjA3Q1W7aVh8nWPz14vuUooN')
 
 export const DONATE_PROGRAM_ID = new PublicKey('tJCoBgdxRUQi6VsUV1ubaWMVkRY9eBASpQhvP9ykrTq')
 
-export const USDC_TOKEN_ADDRESS = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr')
+export const USDC_TOKEN_ADDRESS = new PublicKey('EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v')
 
 export const USDC_TOKEN_DECIMALS = 6
+
+//devnet
+// export const POOL_ADDRESS = new PublicKey('sVA1BdvuBp5xCEUvnNT9uzjVvoQrN77D5nnBKQFEpP1')
+//
+// export const DONATE_PROGRAM_ID = new PublicKey('tJCoBgdxRUQi6VsUV1ubaWMVkRY9eBASpQhvP9ykrTq')
+//
+// export const USDC_TOKEN_ADDRESS = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr')
+//
+// export const USDC_TOKEN_DECIMALS = 6
 
 export * from './idl'
